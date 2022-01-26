fa5c05906cc53a6a1c830882de20be516ea2558b
alexandsten <alex.andsten@hotmail.com>
2022-01-27 00:33:48 +0100

    f

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-var storyTop;
+var storyTop;   // storyn som visas högst upp
 
 var choices1; // knapp 1
 var choices2; // knapp 2
@@ -56,14 +56,8 @@ function createChoices(jsonCode) {
    
 
 
-    for (let i = 0; i < 4; i++) {
+   
     let choiceDiv = document.createElement("div"); // skapa choice div?
-    /*
-    let info = document.createElement("h3");
-    let cityP = document.createElement("p");
-    let ratingP = document.createElement("p");
-    let priceP = document.createElement("p");
-    */
 
     // classes / id
     choiceDiv.setAttribute("class","choices");  // sätt class
@@ -72,23 +66,23 @@ function createChoices(jsonCode) {
 
 
     // text på knappen
-    if (choiceTextNr == 1) {
-       choices1.innerHTML = choiceDiv.innerHTML = stories[0].kapitel[storyChapter].choices.choiceText1; // text för valen
-      choices1.id = stories[0].kapitel[storyChapter].titel.next1;  // id för valen - så man vet vad nästa val är
-       choices1.addEventListener("click",destroyChoices);
-    } else if (choiceTextNr == 2) {
+    
+        choices1.innerHTML = choiceDiv.innerHTML = stories[0].kapitel[storyChapter].choices.choiceText1; // text för valen
+        choices1.id = stories[0].kapitel[storyChapter].titel.next1;  // id för valen - så man vet vad nästa val är
+        choices1.addEventListener("click",destroyChoices);
+  
         choices2.innerHTML = stories[0].kapitel[storyChapter].choices.choiceText2;
         choices2.id = stories[0].kapitel[storyChapter].titel.next2; 
         choices2.addEventListener("click",destroyChoices);
-    } else if (choiceTextNr == 3) {
+  
         choices3.innerHTML = stories[0].kapitel[storyChapter].choices.choiceText3;
         choices3.id = stories[0].kapitel[storyChapter].titel.next3; 
         choices3.addEventListener("click",destroyChoices);
-    } else if (choiceTextNr == 4) {
+  
         choices4.innerHTML = stories[0].kapitel[storyChapter].choices.choiceText4;
         choices4.id = stories[0].kapitel[storyChapter].titel.next4; 
         choices4.addEventListener("click",destroyChoices);
-    }
+  
     ;      // text för svar
 
 
@@ -102,28 +96,13 @@ function createChoices(jsonCode) {
     // eventlisteners
 
     choiceDiv.addEventListener("click",destroyChoices);
-
-    /* add event listener
-    infoDiv.appendChild(saveNode);
-    infoDiv.appendChild(info);
-    infoDiv.appendChild(cityP);
-    infoDiv.appendChild(priceP);
-    infoDiv.appendChild(ratingP)
-    info.appendChild(nameNode);
-    cityP.appendChild(cityNode);
-    ratingP.appendChild(ratingNode);
-    priceP.appendChild(priceNode);
-    info.addEventListener("click",loadInfo) 
-    saveNode.addEventListener("click",saveLocal) 
-    */
-    choiceTextNr++;
-    }
+   
 }
 
 
 function destroyChoices() {
 
-    storyChapter = this.id;
+    storyChapter = this.id; // byt kapitel, den man tryckte på
  
     // kalla på ny story
 
@@ -136,94 +115,3 @@ function destroyChoices() {
 			else document.getElementById("storyText").innerHTML = "Den begärda resursen fanns inte.";}
 
 }
-
-
-
-/*  ingen reload för sidan
-
-function newInfo (responseInfo) {
-    likeBtn2.style.display = "none";
-    response = JSON.parse(responseInfo);
-
-    for (let i = 0; i < response.payload.length; i++) {
-    infoDiv = document.createElement("div");
-    let info = document.createElement("h3");
-    let cityP = document.createElement("p");
-    let ratingP = document.createElement("p");
-    let priceP = document.createElement("p");
-
-    infoDiv.setAttribute("class","resultinfo");
-    info.setAttribute("class","info");
-    info.setAttribute("id",response.payload[i].id);
-    cityP.setAttribute("class","city");
-    ratingP.setAttribute("class","rating");
-
-    let rating = response.payload[i].rating;
-    rating = Math.round(rating);
-
-    let name = response.payload[i].name;
-    let city = response.payload[i].city;
-    let price = "Pris: " + response.payload[i].price_range + "kr";
-
-    let saveNode = document.createElement("img");
-    let nameNode = document.createTextNode(name);
-    let cityNode = document.createTextNode(city);
-    let priceNode = document.createTextNode(price);
-    let ratingNode = document.createElement("img");
-
-    ratingNode.setAttribute("class","rating_s")
-    saveNode.setAttribute("class","likeBtn")
-    saveNode.setAttribute("id", response.payload[i].id)
-    ratingNode.src = "../img/rating/emptystar.svg"
-    
-    let ids=localStorage.getItem("id");
-    if (!ids){
-        ids="";
-    }
-    let idArray=ids.split(',');
-    let isLiked=idArray.includes(response.payload[i].id);
-
-    if (isLiked){
-        saveNode.src="/img/redheart.png";
-    }
-    else {
-        saveNode.src = "/img/heart.png";
-    }
-
-    if (rating == 1) {
-        ratingNode.src = "../img/rating/star1.svg";
-    }
-    if (rating == 2) {
-        ratingNode.src = "../img/rating/star2.svg";
-    }
-
-    if (rating == 3) {
-        ratingNode.src = "../img/rating/star3.svg";
-    }
-    if (rating== 4) {
-        ratingNode.src = "../img/rating/star4.svg";
-    }
-    if (rating == 5) {
-        ratingNode.src = "../img/rating/star4.svg";
-    }
-
-    page1.appendChild(infoDiv)
-    infoDiv.appendChild(saveNode);
-    infoDiv.appendChild(info);
-    infoDiv.appendChild(cityP);
-    infoDiv.appendChild(priceP);
-    infoDiv.appendChild(ratingP)
-    info.appendChild(nameNode);
-    cityP.appendChild(cityNode);
-    ratingP.appendChild(ratingNode);
-    priceP.appendChild(priceNode);
-    info.addEventListener("click",loadInfo) 
-    saveNode.addEventListener("click",saveLocal) 
-    }
-    if (page1.style.display == "none") {
-        prepareMap ();
-    }
-    requestImg ();
-} 
-
-*/
