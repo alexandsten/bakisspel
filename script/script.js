a8f8405a20c366aa80342e7497943680abed404c
alexandsten <alex.andsten@hotmail.com>
2022-01-26 11:58:21 +0100

    s

--- script/script.js
+++ script/script.js
@@ -1,12 +1,21 @@
 var choice1;
 var choice2;
+
 var choice3;
 var choice4;
 
+var choiceText1;
+
 
 function init {
-    
-    choice1 = 
+
+    choice1 = document.getElementById("choiceContainer1");
+    choice2 = document.getElementById("choiceContainer2");
+
+    choiceText1 = <div id="choice1" class="choices">Scream "hello is anyone there!?"" as loud as you can</div>;
+
+    choice1.innerHTML = choiceText1;
+    alert("hello world!");
 }
 
 window.addEventListener("load",init);
