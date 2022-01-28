39e506db2762bf74e8c1baef639ed7f2bdf6aabd
alexandsten <alex.andsten@hotmail.com>
2022-01-28 11:49:46 +0100

    s

--- script/script.js
+++ script/script.js
@@ -52,7 +52,7 @@ function createChoices(jsonCode) {
 
     for (let i = 0; i < stories.length; i++) {
         if (stories[0].kapitel[i].titel.name == storyChapter) {
-            console.log(stories);
+            console.log(storyChapter);
         }
     }
 
