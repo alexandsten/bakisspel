bdae02ccbd09001377b6d3bf8b1516f5d9528e86
alexandsten <alex.andsten@hotmail.com>
2022-02-02 19:09:51 +0100

    s

--- script/script.js
+++ script/script.js
@@ -49,32 +49,109 @@ window.addEventListener("load",init);
 function createChoices(jsonCode) {
 
     let stories = JSON.parse(jsonCode).story;
-    
+
+    // för att randomiza
+    var numbers = ["0", "1", "2", "3"];      // 3 nummer, ett för varje val
+    console.log(numbers);
+    var randomNr;
+
     // testa ett nytt sätt 
 
     for (let i = 0; i < stories[0].kapitel.length; i++) {
-        if (stories[0].kapitel[i].titel.name == storyChapter) {
-            /*
-            lägg in if satser för good/neutral/evil här - så att det blir ett av de tre som visas
-            */
 
-            choices1.innerHTML = stories[0].kapitel[i].choices.choiceText1; // text för valen   
-        /*    choices1.id = stories[0].kapitel[i].titel.next1Text;  // kan ha texten inför nästa val här */
-            storyNext1 = stories[0].kapitel[i].titel.next1Text;
-         
+        randomNr = Math.floor(numbers.length * Math.random());      // väljer ett slumpmässigt nummer som valet ska få
+       
+        
+
+        if (stories[0].kapitel[i].titel.name == storyChapter) {
+            ////// jobbig kod för random val 1
+
+            console.log(randomNr);
+            
+
+            if (randomNr == "0") {
+                choices1.innerHTML = stories[0].kapitel[i].choices.choiceText1;
+                choices2.innerHTML = stories[0].kapitel[i].choices.choiceText2;
+                choices3.innerHTML = stories[0].kapitel[i].choices.choiceText3;
+                //
+                storyNext1 = stories[0].kapitel[i].titel.next1Text;
+                storyNext2 = stories[0].kapitel[i].titel.next1Text;
+                storyNext3 = stories[0].kapitel[i].titel.next1Text;
+            } else if (randomNr == "1") {
+                choices1.innerHTML = stories[0].kapitel[i].choices.choiceText3;
+                choices2.innerHTML = stories[0].kapitel[i].choices.choiceText2;
+                choices3.innerHTML = stories[0].kapitel[i].choices.choiceText1;
+                //
+                storyNext1 = stories[0].kapitel[i].titel.next1Text;
+                storyNext2 = stories[0].kapitel[i].titel.next1Text;
+                storyNext3 = stories[0].kapitel[i].titel.next1Text;
+            } else if (randomNr == "2") {
+                choices1.innerHTML = stories[0].kapitel[i].choices.choiceText3;
+                choices2.innerHTML = stories[0].kapitel[i].choices.choiceText1;
+                choices3.innerHTML = stories[0].kapitel[i].choices.choiceText2;
+                //
+                storyNext1 = stories[0].kapitel[i].titel.next1Text;
+                storyNext2 = stories[0].kapitel[i].titel.next1Text;
+                storyNext3 = stories[0].kapitel[i].titel.next1Text;
+            }   else if (randomNr == "3") {
+                choices1.innerHTML = stories[0].kapitel[i].choices.choiceText2;
+                choices2.innerHTML = stories[0].kapitel[i].choices.choiceText3;
+                choices3.innerHTML = stories[0].kapitel[i].choices.choiceText1;
+                //
+                storyNext1 = stories[0].kapitel[i].titel.next1Text;
+                storyNext2 = stories[0].kapitel[i].titel.next1Text;
+                storyNext3 = stories[0].kapitel[i].titel.next1Text;
+            }
             choices1.addEventListener("click",nextChoices);
-      
-            choices2.innerHTML = stories[0].kapitel[i].choices.choiceText2;
-        /*    choices2.id = stories[0].kapitel[i].titel.next2;  */
-            storyNext2 = stories[0].kapitel[i].titel.next1Text;
             choices2.addEventListener("click",nextChoices);
-      
-            choices3.innerHTML = stories[0].kapitel[i].choices.choiceText3;
-        /*    choices3.id = stories[0].kapitel[i].titel.next3; */
-            storyNext3 = stories[0].kapitel[i].titel.next1Text;
             choices3.addEventListener("click",nextChoices);
 
             storyTop.innerHTML = stories[0].kapitel[i].story.storyText;  // texten på toppen av skärmen
+            ////////////////////////////////////////////////
+
+
+
+
+            // för val 1
+            /*
+            lägg in if satser för good/neutral/evil här - så att det blir ett av de tre som visas
+            */
+           /*
+            
+           randomNr = Math.floor(numbers.length * Math.random());      // väljer ett slumpmässigt nummer som valet ska få
+            console.log(randomNr);
+            console.log(numbers);
+
+            choices1.innerHTML = stories[0].kapitel[i].choices.choiceText1; // text för valen   
+            choices1.id = stories[0].kapitel[i].titel.next1Text;  // kan ha texten inför nästa val här
+            storyNext1 = stories[0].kapitel[i].titel.next1Text;
+            numbers.splice(randomNr,1);     // ta bort detta nummer, så brickor inte får samma nr
+         
+            choices1.addEventListener("click",nextChoices);
+
+            // för val 2
+            randomNr = Math.floor(numbers.length * Math.random());      // väljer ett slumpmässigt nummer som valet ska få
+            console.log(randomNr);
+            console.log(numbers);
+            choices2.innerHTML = stories[0].kapitel[i].choices.choiceText2;
+           choices2.id = stories[0].kapitel[i].titel.next2;  
+            storyNext2 = stories[0].kapitel[i].titel.next1Text;
+            numbers.splice(randomNr,1);     // ta bort detta nummer, så brickor inte får samma nr
+            choices2.addEventListener("click",nextChoices);
+
+            // för val 3
+            randomNr = Math.floor(numbers.length * Math.random());      // väljer ett slumpmässigt nummer som valet ska få
+            console.log(randomNr);
+            console.log(numbers);
+            choices3.innerHTML = stories[0].kapitel[i].choices.choiceText3;
+           choices3.id = stories[0].kapitel[i].titel.next3; 
+            storyNext3 = stories[0].kapitel[i].titel.next1Text;
+            numbers.splice(randomNr,1);     // ta bort detta nummer, så brickor inte får samma nr
+            choices3.addEventListener("click",nextChoices);
+
+            storyTop.innerHTML = stories[0].kapitel[i].story.storyText;  // texten på toppen av skärmen
+
+            */
         }
     }
 }
@@ -86,13 +163,13 @@ function nextChoices() {
    
 // vilken story som ska visas innan nästa "val", kan även välja vilken av de 3 seten av val som ska visas vid nästa val
     if (this.id == "choices1") {
-        alert(storyNext1)
+        alert("choice 1 " + storyNext1);
         /* nästa val = 1 / de goda valen */
     } else if (this.id == "choices2") {
-        alert(storyNext2)
+        alert("choice 2 " + storyNext2);
          /* nästa val = 2 / de neutrala valen */
     }   else if (this.id == "choices2") {
-        alert(storyNext3)
+        alert("choice 3 " + storyNext3);
          /* nästa val = 3 / de onda valen */
     }
 
